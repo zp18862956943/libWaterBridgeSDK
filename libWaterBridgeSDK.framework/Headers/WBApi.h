330a5a483ec5b3b4ed50a510342264625edc01d1
周鹏 <18862956943@163.com>
2020-10-19 12:41:46 +0800

    添加 多环境

--- libWaterBridgeSDK.framework/Headers/WBApi.h
+++ libWaterBridgeSDK.framework/Headers/WBApi.h
@@ -13,12 +13,18 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+typedef enum : NSUInteger {
+    SystemEnvironmentDev,
+    SystemEnvironmentUat,
+    SystemEnvironmentRelease,
+} SystemEnvironment;
+
 @protocol WBApiDelegate <NSObject>
 @optional
 
-/*! @brief 发送一个sendReq后，收到微信的回应
+/*! @brief 发送一个sendReq后，收到水桥的回应
  *
- * 收到一个来自微信的处理结果。调用一次sendReq后会收到onResp。
+ * 收到一个来自水桥的处理结果。调用一次sendReq后会收到onResp。
  * 可能收到的处理结果有SendMessageToWXResp、SendAuthResp等。
  * @param resp 具体的回应内容，是自动释放的
  */
@@ -34,18 +40,23 @@ NS_ASSUME_NONNULL_BEGIN
  */
 + (BOOL)isInstalled;
 
-/*! @brief WBApi的成员函数，向微信终端程序注册第三方应用。
+/**
+ 设置开发模式:SystemEnvironmentDev 测试环境,SystemEnvironmentUat 预发环境, SystemEnvironmentRelease 生产环境
+ */
++ (void)setDevModel:(SystemEnvironment )environmentType;
+
+/*! @brief WBApi的成员函数，向水桥终端程序注册第三方应用。
 */
 + (BOOL)registerApp:(NSString *)appid;
 
 /// 调起水桥程序
 +(void)sendAuthRequestWithCompletion:(void (^)(NSError * _Nullable error))completion;
 
-/*! @brief 处理微信通过URL启动App时传递的数据
+/*! @brief 处理水桥通过URL启动App时传递的数据
  *
  * 需要在 application:openURL:sourceApplication:annotation:或者application:handleOpenURL中调用。
- * @param url 微信启动第三方应用时传递过来的URL
- * @param delegate  WBApiDelegate对象，用来接收微信触发的消息。
+ * @param url 水桥启动第三方应用时传递过来的URL
+ * @param delegate  WBApiDelegate对象，用来接收水桥触发的消息。
  * @return 成功返回YES，失败返回NO。
  */
 + (BOOL)handleOpenURL:(NSURL *)url delegate:(nullable id<WBApiDelegate>)delegate;
